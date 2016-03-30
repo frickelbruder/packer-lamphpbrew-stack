c4a5082e8c32645067f726b84d62203174d1400c
maggus <mah@netvisio.de>
2016-03-31 00:29:33 +0200

    php-fpm in autostart xdebug.ini-path add mod_fastcgi smaller additions

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -11,22 +11,23 @@ apt-get install -y percona-server-server
 
 #Apache2
 printf $ECHOWRAPPER "Installing Apache"
-apt-get install -y apache2-utils libapache2-mod-fastcgi
+apt-get install -y apache2 apache2-utils libapache2-mod-fastcgi
 a2enmod actions fastcgi alias rewrite
-a2dismod php5
 service apache2 restart
+
 
 #PHP
 printf $ECHOWRAPPER "Installing PHPBrew"
 apt-get build-dep php5
-apt-get install -y php5 php5-dev php-pear autoconf automake curl libcurl3-openssl-dev build-essential libxslt1-dev re2c libxml2 libxml2-dev php5-cli bison libbz2-dev libreadline-dev
-apt-get install -y libfreetype6 libfreetype6-dev libpng12-0 libpng12-dev libjpeg-dev libjpeg8-dev libjpeg8  libgd-dev libgd3 libxpm4 libltdl7 libltdl-dev
-apt-get install -y libssl-dev openssl
-apt-get install -y gettext libgettextpo-dev libgettextpo0
-apt-get install -y libicu-dev
-apt-get install -y libmhash-dev libmhash2
-apt-get install -y libmcrypt-dev libmcrypt4
-apt-get install -y mysql-server mysql-client libmysqlclient-dev libmysqld-dev
+apt-get install -y php5 php5-dev php-pear autoconf automake curl libcurl3-openssl-dev build-essential libxslt1-dev re2c libxml2 libxml2-dev php5-cli bison libbz2-dev libreadline-dev \
+            libfreetype6 libfreetype6-dev libpng12-0 libpng12-dev libjpeg-dev libjpeg8-dev libjpeg8  libgd-dev libgd3 libxpm4 libltdl7 libltdl-dev \
+             libssl-dev openssl \
+             gettext libgettextpo-dev libgettextpo0 \
+             libicu-dev \
+             libmhash-dev libmhash2 \
+             libmcrypt-dev libmcrypt4 \
+             libmysqlclient-dev libmysqld-dev
+a2dismod php5
 
 
 printf $ECHOWRAPPER Installing PHPBrew
@@ -51,13 +52,11 @@ echo "export PHPBREW_HOME=/opt/phpbrew" >> /etc/bash.bashrc
 echo "source /opt/phpbrew/bashrc" >> /etc/bash.bashrc
 
 chown -R root: /opt/phpbrew    
-sed -i 's/ = nobody/ = www-data/' /opt/phpbrew/php/php-5.6.19/etc/php-fpm.conf                               
+sed -i 's/ = nobody/ = www-data/' /opt/phpbrew/php/php-5.6.19/etc/php-fpm.conf
+sed -i 's/;date.timezone =/date.timezone = Europe\/Berlin/' /opt/phpbrew/php/php-5.6.19/etc/php.ini                                        
 
 phpbrew switch 5.6.19 
 phpbrew ext install xdebug stable
-phpbrew fpm start
-
-echo "phpbrew fpm start" >> /etc/rc.local
    
 ##PHPDEVELOPMENT
 ###PHPUNIT
@@ -92,6 +91,8 @@ debconf-set-selections <<< 'phpmyadmin phpmyadmin/reconfigure-webserver multisel
 debconf-set-selections <<< 'phpmyadmin phpmyadmin/internal/skip-preseed boolean true '
 printf $ECHOWRAPPER "Doing the install"
 apt-get install -y phpmyadmin
+sed -i 's~ //\(.*AllowNoPassword.*\)~\1~1' /etc/phpmyadmin/config.inc.php
+
 
 #Ruby (required for compass)
 printf $ECHOWRAPPER "Installing Ruby"
